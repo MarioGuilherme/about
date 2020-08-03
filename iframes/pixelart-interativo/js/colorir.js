5a20c9d780b32f4adf31f6597ba2c3c622a7aa82
MarioGuilherme <marioguietec@gmail.com>
2020-08-03 03:02:15 -0300

    Correção permanente do Jumbotron e Iframes

---
+++ iframes/pixelart-interativo/js/colorir.js
@@ -0,0 +1,53 @@
+var CorCaneta = "white"
+
+function corBranca() {
+    CorCaneta = "white"
+}
+
+function corPreto() {
+    CorCaneta = "black"
+}
+
+function corAzul() {
+    CorCaneta = "blue"
+}
+
+function corVerde() {
+    CorCaneta = "green"
+}
+
+function corVermelha() {
+    CorCaneta = "red"
+}
+
+function corAmarela() {
+    CorCaneta = "yellow"
+}
+
+function corRoxo() {
+    CorCaneta = "purple"
+}
+
+function corLaranja() {
+    CorCaneta = "orange"
+}
+
+function corBege() {
+    CorCaneta = "#f9eac3"
+}
+
+function corMarrom() {
+    CorCaneta = "#785027"
+}
+
+function corRosa() {
+    CorCaneta = "pink"
+}
+
+function Apagar() {
+    CorCaneta = "#333"
+}
+
+function setPixelColour(pixel) {
+    pixel.style.backgroundColor = CorCaneta
+}
