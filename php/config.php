3015ddc637cb2c301a28b8855bac132e3a4c8440
MarioGuilherme <marioguietec@gmail.com>
2021-01-18 20:51:15 -0300

    Correções

--- php/config.php
+++ php/config.php
@@ -1,14 +1,14 @@
 <?php
 
     date_default_timezone_set("America/Sao_Paulo");
-
-    $host = 'sql306.epizy.com';
-    $user = 'epiz_27016753';
-    $senha = 'mgvJ2WIp42y9k';
-    $banco = 'epiz_27016753_contato';
-
-/*  ----- PARA BANCO LOCAL -------
     $host = 'localhost';
     $user = 'root';
     $senha = 'root';
     $banco = 'portfolio-contato';
+ 
+
+/*  ----- PARA BANCO LOCAL -------
+     $host = 'sql306.epizy.com';
+    $user = 'epiz_27016753';
+    $senha = 'mgvJ2WIp42y9k';
+    $banco = 'epiz_27016753_contato';
--- php/ip.php
+++ php/ip.php
@@ -11,9 +11,17 @@
     $linhas = mysqli_num_rows($resultado);
 
     if($linhas > 0){
-        exit;
         $registrado = 'sim';
+        exit;
     }else{
         $sql_insert = "INSERT INTO ips (ip) VALUES ('$ip')";
         $resultado_insert = mysqli_query($conexao, $sql_insert);
     }
+
+    if($resultado_insert){
+       $dados = array('deu bom');
+    } else {
+        $dados = array('deu bom',
+     mysqli_error($resultado_insert));
+    }   
+    echo json_encode($dados, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
